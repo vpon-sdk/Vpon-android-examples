210728a8427ab8dd4e18675507afa2cf80955eac
ArtCheng <art.cheng@vpon.com>
2020-03-18 15:01:10 +0800

    add native ad sample code ,change to admob adapter v2.0.0, vpon sdk v5.0.2

--- admobexample/src/main/java/com/vpon/admobexample/MainActivity.java
+++ admobexample/src/main/java/com/vpon/admobexample/MainActivity.java
@@ -3,30 +3,44 @@ package com.vpon.admobexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AdView adView;
     private InterstitialAd interstitial;
+    private AdLoader adLoader;
+    private UnifiedNativeAdView unifiedNativeAdView;
     private String MY_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";//TODO SET YOUR AD_UNIT_ID here
     private String MY_INTERSTITIAL_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";////TODO SET YOUR AD_UNIT_ID here
-    private LinearLayout adBannerLayout;
+    private String MY_NATIVE_UNIT_ID = "ca-app-pub-3940256099942544/2247696110";////TODO SET YOUR AD_UNIT_ID here
+    private LinearLayout adLayout;
 
 
     // In this sample, we will show you the banner and IS ads
     // by onclicks. Don't forget to import the SDK!
-    private Button button1;
-    private Button button2;
+    private Button BannerButton;
+    private Button InterstitialButton;
+    private Button NativeAdButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +50,27 @@ public class MainActivity extends AppCompatActivity {
         MobileAds.initialize(this);
 
         // button1 for banner and button for IS
-        button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        BannerButton = findViewById(R.id.banner_button);
+        BannerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doBannerAd();
             }
         });
 
-        button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        InterstitialButton = findViewById(R.id.is_button);
+        InterstitialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doInterstitialAd();
+            }
+        });
+
+        NativeAdButton = findViewById(R.id.native_ad_button);
+        NativeAdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doNativeAd();
             }
         });
 
@@ -64,12 +86,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the AdView to the view hierarchy. The view will have no size
         // until the ad is loaded.
-        adBannerLayout = findViewById(R.id.container);
-        adBannerLayout.addView(adView);
+        adLayout = findViewById(R.id.container);
+        adLayout.addView(adView);
         // start loading the ad in the background
+
+        unifiedNativeAdView = (UnifiedNativeAdView) LayoutInflater.from(this).inflate(R.layout.layout_native_ad_template1, null);
+        adLayout.addView(unifiedNativeAdView);
     }
 
-    private void doBannerAd(){
+    private void doBannerAd() {
         AdRequest adRequest = new AdRequest.Builder()
                 .build();
         adView.loadAd(adRequest);
@@ -102,26 +127,62 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         interstitial.setAdListener(new AdListener() {
             @Override
-            public void onAdLoaded(){
+            public void onAdLoaded() {
                 interstitial.show();
             }
 
             @Override
-            public void onAdFailedToLoad(int errorCode){}
+            public void onAdFailedToLoad(int errorCode) {
+            }
 
             @Override
-            public void onAdOpened(){}
+            public void onAdOpened() {
+            }
 
             @Override
-            public void onAdClosed(){}
+            public void onAdClosed() {
+            }
 
             @Override
-            public void onAdLeftApplication(){}
+            public void onAdLeftApplication() {
+            }
         });
         interstitial.loadAd(request);
     }
 
-    /** Called before the activity is destroyed. */
+    private void doNativeAd() {
+        adLoader = new AdLoader.Builder(this, MY_NATIVE_UNIT_ID)
+                .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
+                    @Override
+                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
+                        // Show the ad.
+                        populateUnifiedNativeAdView(unifiedNativeAd, unifiedNativeAdView);
+                    }
+                })
+                .withAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                        // Handle the failure by logging, altering the UI, and so on.
+                        Log.d("main", "fail to load " + errorCode);
+                    }
+
+                    @Override
+                    public void onAdOpened() {
+                        super.onAdOpened();
+                    }
+                })
+                .withNativeAdOptions(new NativeAdOptions.Builder()
+                        // Methods in the NativeAdOptions.Builder class can be
+                        // used here to specify individual options settings.
+                        .build())
+                .build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+    /**
+     * Called before the activity is destroyed.
+     */
     @Override
     public void onDestroy() {
         // Destroy the AdView.
@@ -130,5 +191,84 @@ public class MainActivity extends AppCompatActivity {
             adView = null;
         }
         super.onDestroy();
+    }
+
+    private void populateUnifiedNativeAdView(UnifiedNativeAd nativeAd, UnifiedNativeAdView adView) {
+        adView.setVisibility(View.VISIBLE);
+        // Set the media view. Media content will be automatically populated in the media view once
+        // adView.setNativeAd() is called.
+        MediaView mediaView = adView.findViewById(R.id.ad_media);
+        adView.setMediaView(mediaView);
+
+        // Set other ad assets.
+        adView.setHeadlineView(adView.findViewById(R.id.ad_headline));
+        adView.setBodyView(adView.findViewById(R.id.ad_body));
+        adView.setCallToActionView(adView.findViewById(R.id.ad_call_to_action));
+        adView.setIconView(adView.findViewById(R.id.ad_app_icon));
+        adView.setAdvertiserView(adView.findViewById(R.id.ad_advertiser));
+        adView.setStarRatingView(adView.findViewById(R.id.ad_stars));
+        adView.setStoreView(adView.findViewById(R.id.ad_store));
+        adView.setPriceView(adView.findViewById(R.id.ad_price));
+
+        // The headline is guaranteed to be in every UnifiedNativeAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAd.getHeadline());
+
+        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
+        // check before trying to display them.
+        if (nativeAd.getBody() == null) {
+            adView.getBodyView().setVisibility(View.GONE);
+        } else {
+            adView.getBodyView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
+        }
+
+        if (nativeAd.getCallToAction() == null) {
+            adView.getCallToActionView().setVisibility(View.GONE);
+        } else {
+            adView.getCallToActionView().setVisibility(View.VISIBLE);
+            ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
+        }
+
+        if (nativeAd.getIcon() == null) {
+            adView.getIconView().setVisibility(View.GONE);
+        } else {
+            ((ImageView) adView.getIconView()).setImageDrawable(
+                    nativeAd.getIcon().getDrawable());
+            adView.getIconView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.GONE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAd.getPrice());
+        }
+
+        if (nativeAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.GONE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAd.getStore());
+        }
+
+        if (nativeAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.GONE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getAdvertiser() == null) {
+            adView.getAdvertiserView().setVisibility(View.GONE);
+        } else {
+            ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
+            adView.getAdvertiserView().setVisibility(View.VISIBLE);
+        }
+
+        // This method tells the Google Mobile Ads SDK that you have finished populating your
+        // native ad view with this native ad. The SDK will populate the adView's MediaView
+        // with the media content from this native ad.
+        adView.setNativeAd(nativeAd);
     }
 }
